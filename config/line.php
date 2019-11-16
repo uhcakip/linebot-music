d37fc3caa1a71de95742926b847abc076f25ddf8
Yu <kuxox5150@gmail.com>
2019-11-16 15:55:55 +0800

    [Update] 加入 Line Developer 相關設定

---
+++ config/line.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'token' => env('LINEBOT_TOKEN', ''),
+    'id' => env('LINEBOT_CHANNEL_ID', ''),
+    'secret' => env('LINEBOT_CHANNEL_SECRET', ''),
+    'user' => env('LINEBOT_USER_ID', ''),
+    'rich_menu_id' => env('LINEBOT_RICH_MENU_ID', ''),
+];
