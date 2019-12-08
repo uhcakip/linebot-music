3c4e77699f453735e5c353f1fb25c756dd67c207
Yu <kuxox5150@gmail.com>
2019-12-09 00:55:10 +0800

    [Add] 新增 Rich Menu 相關功能

---
+++ app/Console/Commands/RichMenuManage.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\RichMenuService;
+use Illuminate\Console\Command;
+
+class RichMenuManage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'linebot:richmenu {action}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '管理 Rich Menu';
+
+    protected $richMenuService;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param RichMenuService $richMenuService
+     */
+    public function __construct(RichMenuService $richMenuService)
+    {
+        parent::__construct();
+
+        // 注入
+        $this->richMenuService = $richMenuService;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        switch ($this->argument('action')) {
+            case 'create':
+                $this->richMenuService->create();
+                break;
+            case 'delete':
+                $this->richMenuService->delete();
+                break;
+        }
+
+        $this->info('執行完成');
+    }
+}
---
+++ app/Services/RichMenuService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\RichMenuBuilder;
+use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBoundsBuilder;
+use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBuilder;
+use LINE\LINEBot\RichMenuBuilder\RichMenuSizeBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+
+class RichMenuService
+{
+    protected $httpClient;
+    protected $lineBot;
+
+    public function __construct()
+    {
+        $this->httpClient = new CurlHTTPClient(config('line.token'));
+        $this->lineBot = new LINEBot($this->httpClient, ['channelSecret' => config('line.secret')]);
+    }
+
+    public function create()
+    {
+        // size
+        $size = new RichMenuSizeBuilder(843, 2500);
+        // bounds
+        $bounds = [
+            new RichMenuAreaBoundsBuilder(0, 0, 833, 843),
+            new RichMenuAreaBoundsBuilder(833, 0, 833, 843),
+            new RichMenuAreaBoundsBuilder(1666, 0, 833, 843),
+        ];
+        // actions
+        $actions = [
+            new PostbackTemplateActionBuilder('singer', 'singer', '請輸入歌手名'),
+            new PostbackTemplateActionBuilder('song', 'song', '請輸入歌曲名'),
+            new PostbackTemplateActionBuilder('album', 'album', '請輸入專輯名'),
+        ];
+        // areas
+        $areas = [];
+        for ($i = 0; $i < count($bounds); $i++) {
+            $areas[] = new RichMenuAreaBuilder($bounds[$i], $actions[$i]);
+        }
+
+        // create
+        $richMenu = new RichMenuBuilder($size, true, 'linebot-music', '選擇搜尋範圍', $areas);
+        $response = $this->lineBot->createRichMenu($richMenu);
+        // 建立完會回傳一組 richMenuId
+        $richMenuId = $response->getJSONDecodedBody()['richMenuId'];
+
+        // upload image
+        $this->lineBot->uploadRichMenuImage($richMenuId, 'public/rich_menu_img/line-rich-menu.png', 'image/png');
+
+        // link -> 完成後就可以用 getRichMenuId() 取得 richMenuId
+        $this->lineBot->linkRichMenu(config('line.user'), $richMenuId);
+    }
+
+    public function delete()
+    {
+        $this->lineBot->unlinkRichMenu(config('line.user'));
+        $this->lineBot->deleteRichMenu(config('line.user'));
+    }
+}
